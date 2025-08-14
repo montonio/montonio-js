65060ce63be7001c49926d7300fb480963bf8e99
kalluste <karl.kalluste@gmail.com>
2025-08-14 12:15:40 +0300

    Expose ErrorEnum

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { MontonioSDKExceptionEnum } from './common';
+import { ErrorEnum } from './common';
 import { MontonioCheckout } from './components';
 import type { CheckoutOptions } from './components/MontonioCheckout/types';
 import { LocaleEnum } from './components/MontonioCheckout/types';
 import { Environment } from './services/Config/types';
 import { MessageTypeEnum } from './services/Messaging';
 
-export { CheckoutOptions, MontonioCheckout, Environment, MessageTypeEnum, LocaleEnum, MontonioSDKExceptionEnum };
+export { CheckoutOptions, MontonioCheckout, Environment, MessageTypeEnum, LocaleEnum, ErrorEnum };
